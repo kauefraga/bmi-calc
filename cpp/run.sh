ffd6130c2f77a2205de50360764ab9b6405afdbc
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-20 23:20:09 -0300

    :hammer: script: add compiler/runner

---
+++ cpp/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#   /$$   /$$ /$$$$$$$$
+#  | $$  /$$/| $$_____/
+#  | $$ /$$/ | $$
+#  | $$$$$/  | $$$$$
+#  | $$  $$  | $$__/
+#  | $$\  $$ | $$
+#  | $$ \  $$| $$      Author: Kauê Fraga Rodrigues
+#  |__/  \__/|__/      github.com/kauefraga/bmi-calc
+
+# compile `main.cpp` and run the g++ output
+main="cpp/main"
+
+g++ $main.cpp -o $main -std=c++20
+$main
