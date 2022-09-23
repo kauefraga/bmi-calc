b541f854453852c746804c5d3381a6d704e6a42e
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-22 21:23:06 -0300

    :recycle: chore: include header file

---
+++ cpp/lib.hpp
@@ -0,0 +1,6 @@
+float calcBMI(float weight, float height)
+{
+  return weight / (height * height) * 10000; // round with *10000
+}
+
+// TODO: argsParser
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
+#include "lib.hpp"
 
-// BMI formula is weight over squared height (w / (h*h))
-float calcBMI(float weight, float height) {
-  return weight / (height * height) * 10000; // round with *10000
-}
-
-int main() {
+int main()
+{
   std::cout << ">> Calculator of Body Mass Index <<\n\n";
-
 
   float bmi, weight, height;
 
   std::cout << "Whats your weight? (kg) > ";
   std::cin >> weight;
-
 
   std::cout << "Whats your height? (cm) > ";
   std::cin >> height;
